285d2e9fc302a5dae4e3888fdefc5bc66d4366c5
Anastassia Lobatsjova <anastassia.lobatsjova@coolbet.com>
2025-08-11 18:17:40 +0300

    add-endpoints

---
+++ posts/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+app.use(express.json())
+const { randomBytes } = require('crypto');
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+})
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+    posts[id] = { id, title };
+    res.status(201).send(posts[id]);
+})
+
+app.listen(4000, () => {
+    console.log('Listening on 4000');
+})
